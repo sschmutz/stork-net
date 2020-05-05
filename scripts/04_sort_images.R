01e8f7d070f2792f170a3df900f7f62167463939
sschmutz <stefan.schmutz@outlook.com>
2020-05-05 17:06:32 +0200

    change train ratio and include validation samples already

--- scripts/04_sort_images.R
+++ scripts/04_sort_images.R
@@ -2,12 +2,22 @@ library(tidyverse)
 library(here)
 
 # with this script, the labeled images are sorted into folders
-# 2019_train/stork/ and 2019_train/no_stork/ are the training sets
+# 2019_train/train/stork/ and 2019_train/train/no_stork/ are the training sets
+# 2019_train/validation/stork/ and 2019_train/validation/no_stork/ are the validation sets
 # 2019_test/ ist the test set
 
-# of all the labeled images 75% were sampled for the training set
+# in the end there should be 60 test images, 340 training images (of which 60 are to validate, and 280 are to train)
+
+# the datastructure follows roughly the one used in this tutorial:
+# https://www.tensorflow.org/tutorials/images/classification
+
+# of all the labeled images 85% were sampled for the training set
 # it was not considered if a stork is present or not (no stratified sampling)
-ratio_train <- 0.75
+# during training 15% of those 85% will be used as validation set
+ratio_train <- 0.85
+
+# of the training set, 17.6% can be used for validation
+ratio_validate <- 0.177
 
 # to make it reproducible, a seed was set
 set.seed(20200505)
@@ -20,9 +30,14 @@ labeled_train <-
   labeled %>%
   sample_n(nrow(labeled) * ratio_train)
 
+labeled_validation <-
+  labeled_train %>%
+  sample_n(nrow(labeled_train) * ratio_validate)
+
 labeled <-
   labeled %>%
-  mutate(train = if_else(filename %in% labeled_train$filename, 1, 0))
+  mutate(train = if_else(filename %in% labeled_train$filename, 1, 0),
+         validation = if_else(filename %in% labeled_validation$filename, 1, 0))
 
 # overwrite the csv file with the updated version which contains an additional column (train yes or no)
 write_csv(labeled, here("images", "files_2019_sampled_labeled.csv"))
@@ -32,11 +47,25 @@ write_csv(labeled, here("images", "files_2019_sampled_labeled.csv"))
 train_stork <-
   labeled %>%
   filter(train == 1,
+         validation == 0,
          stork_present == 1)
 
 train_no_stork <-
   labeled %>%
   filter(train == 1,
+         validation == 0,
+         stork_present == 0)
+
+validation_stork <-
+  labeled %>%
+  filter(train == 1,
+         validation == 1,
+         stork_present == 1)
+
+validation_no_stork <-
+  labeled %>%
+  filter(train == 1,
+         validation == 1,
          stork_present == 0)
 
 test <-
@@ -45,13 +74,21 @@ test <-
 
 
 # copy the files into the respective folders (the original images were kept)
+# train
 file.copy(from = here("images", "2019_sampled", train_stork$filename),
-          to = here("images", "2019_train", "stork", train_stork$filename))
+          to = here("images", "2019_train", "train", "stork", train_stork$filename))
 
 file.copy(from = here("images", "2019_sampled", train_no_stork$filename),
-          to = here("images", "2019_train", "no_stork", train_no_stork$filename))
+          to = here("images", "2019_train", "train", "no_stork", train_no_stork$filename))
 
+# validation
+file.copy(from = here("images", "2019_sampled", validation_stork$filename),
+          to = here("images", "2019_train", "validation", "stork", validation_stork$filename))
 
+file.copy(from = here("images", "2019_sampled", validation_no_stork$filename),
+          to = here("images", "2019_train", "validation", "no_stork", validation_no_stork$filename))
+
+# test
 file.copy(from = here("images", "2019_sampled", test$filename),
           to = here("images", "2019_test", test$filename))
 
