ac3d10afa43dcafe166fbcd3df8aa5e67cd30e90
sschmutz <stefan.schmutz@outlook.com>
2020-05-13 14:29:54 +0200

    adapt scripts to new sampling/labeling

--- scripts/04_sort-images.R
+++ scripts/04_sort-images.R
@@ -2,11 +2,11 @@ library(tidyverse)
 library(here)
 
 # with this script, the labeled images are sorted into folders
-# 2019_train/train/stork/ and 2019_train/train/no_stork/ are the training sets
-# 2019_train/validation/stork/ and 2019_train/validation/no_stork/ are the validation sets
+# 2019_train/train/0_stork/, 1_stork/, 2_stork/, 3_stork/ are the training sets
+# 2019_train/validation/0_stork/, 1_stork/, 2_stork/, 3_stork/  are the validation sets
 # 2019_test/ ist the test set
 
-# in the end there should be 60 test images, 340 training images (of which 60 are to validate, and 280 are to train)
+# in the end there should be 441 test images, 2499 training images (of which 442 are to validate, and 2057 are to train)
 
 # the datastructure follows roughly the one used in this tutorial:
 # https://www.tensorflow.org/tutorials/images/classification
@@ -24,7 +24,8 @@ set.seed(20200505)
 
 
 labeled <-
-  read_csv(here("images", "files_2019_sampled_labeled.csv"))
+  read_csv(here("images", "files_2019_sampled_labeled.csv")) %>%
+  filter(!is.na(stork_number))
 
 labeled_train <-
   labeled %>%
@@ -44,29 +45,54 @@ write_csv(labeled, here("images", "files_2019_sampled_labeled.csv"))
 
 
 # filter for the samples which should be copied into the individual folders
-train_stork <-
+train_0_stork <-
   labeled %>%
   filter(train == 1,
          validation == 0,
-         stork_present == 1)
+         stork_number == 0)
 
-train_no_stork <-
+train_1_stork <-
   labeled %>%
   filter(train == 1,
          validation == 0,
-         stork_present == 0)
+         stork_number == 1)
 
-validation_stork <-
+train_2_stork <-
+  labeled %>%
+  filter(train == 1,
+         validation == 0,
+         stork_number == 2)
+
+train_3_stork <-
+  labeled %>%
+  filter(train == 1,
+         validation == 0,
+         stork_number == 3)
+
+
+validation_0_stork <-
   labeled %>%
   filter(train == 1,
          validation == 1,
-         stork_present == 1)
+         stork_number == 0)
 
-validation_no_stork <-
+validation_1_stork <-
   labeled %>%
   filter(train == 1,
          validation == 1,
-         stork_present == 0)
+         stork_number == 1)
+
+validation_2_stork <-
+  labeled %>%
+  filter(train == 1,
+         validation == 1,
+         stork_number == 2)
+
+validation_3_stork <-
+  labeled %>%
+  filter(train == 1,
+         validation == 1,
+         stork_number == 3)
 
 test <-
   labeled %>%
@@ -75,18 +101,33 @@ test <-
 
 # copy the files into the respective folders (the original images were kept)
 # train
-file.copy(from = here("images", "2019_sampled", train_stork$filename),
-          to = here("images", "2019_train", "train", "stork", train_stork$filename))
+file.copy(from = here("images", "2019_sampled", train_0_stork$filename),
+          to = here("images", "2019_train", "train", "0_stork", train_0_stork$filename))
 
-file.copy(from = here("images", "2019_sampled", train_no_stork$filename),
-          to = here("images", "2019_train", "train", "no_stork", train_no_stork$filename))
+file.copy(from = here("images", "2019_sampled", train_1_stork$filename),
+          to = here("images", "2019_train", "train", "1_stork", train_1_stork$filename))
+
+file.copy(from = here("images", "2019_sampled", train_2_stork$filename),
+          to = here("images", "2019_train", "train", "2_stork", train_2_stork$filename))
+
+file.copy(from = here("images", "2019_sampled", train_3_stork$filename),
+          to = here("images", "2019_train", "train", "3_stork", train_3_stork$filename))
+
+
 
 # validation
-file.copy(from = here("images", "2019_sampled", validation_stork$filename),
-          to = here("images", "2019_train", "validation", "stork", validation_stork$filename))
+file.copy(from = here("images", "2019_sampled", validation_0_stork$filename),
+          to = here("images", "2019_train", "validation", "0_stork", validation_0_stork$filename))
 
-file.copy(from = here("images", "2019_sampled", validation_no_stork$filename),
-          to = here("images", "2019_train", "validation", "no_stork", validation_no_stork$filename))
+file.copy(from = here("images", "2019_sampled", validation_1_stork$filename),
+          to = here("images", "2019_train", "validation", "1_stork", validation_1_stork$filename))
+
+file.copy(from = here("images", "2019_sampled", validation_2_stork$filename),
+          to = here("images", "2019_train", "validation", "2_stork", validation_2_stork$filename))
+
+file.copy(from = here("images", "2019_sampled", validation_3_stork$filename),
+          to = here("images", "2019_train", "validation", "3_stork", validation_3_stork$filename))
+
 
 # test
 file.copy(from = here("images", "2019_sampled", test$filename),
